8bc27e683c648f62827d22cb0c2abcf4627a02d0
Anupa Perera <anupaperera112@gmail.com>
2024-06-10 12:17:48 +0530

    basic structure

---
+++ app/layout.jsx
@@ -0,0 +1,24 @@
+import '@styles/globals.css'
+import { Children } from 'react'
+
+export const metadata = {
+    title: "Promptopia",
+    description: "Discover & Share AI Prompts"
+}
+
+const RootLayout = ({Children}) => {
+  return (
+    <html lang='en'>
+        <body>
+            <div className='main'>
+                <div className='gradient'/>
+            </div>
+            <main className='app'>
+                {Children}
+            </main>
+        </body>
+    </html>
+  )
+}
+
+export default RootLayout
